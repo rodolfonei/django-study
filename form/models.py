62e6eacf12648e865e46d0ec9e9190e7e5a45bc7
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2021-12-02 19:42:13 -0300

    create request model and migrations

--- form/models.py
+++ form/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class Request(models.Model):
+    description = models.TextField(max_length=30)
+    email = models.EmailField(max_length=255)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    send_mail = models.BooleanField(default=True)
+
+    MOTIVATION_CHOICES = (
+        ('Vacation', 'Vacation'),
+        ('Holiday', 'Holiday'),
+        ('Sick', 'Sick')
+    )
+
+    motivation = models.CharField(max_length=60, choices=MOTIVATION_CHOICES)
+    
+
+
